54d366681da127cb1c526948678acbc2186a0eca
lindatrosken <linda.troesken@gmx.net>
2021-09-07 11:02:13 +0200

    Add Swagger

--- backend/src/main/java/de/lindatroesken/backend/api/User.java
+++ backend/src/main/java/de/lindatroesken/backend/api/User.java
@@ -2,6 +2,7 @@ package de.lindatroesken.backend.api;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,5 +10,7 @@ import lombok.Data;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
+
+    @ApiModelProperty(required = true, example = "max123", notes = "The username of the user")
     private String username;
 }
