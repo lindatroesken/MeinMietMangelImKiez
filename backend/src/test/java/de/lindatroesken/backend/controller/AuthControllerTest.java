30415079fd25425082cd076f3d42f43a2421acab
lindatrosken <linda.troesken@gmx.net>
2021-09-08 12:23:35 +0200

    Refactor Test for AuthController with real token

--- backend/src/test/java/de/lindatroesken/backend/controller/AuthControllerTest.java
+++ backend/src/test/java/de/lindatroesken/backend/controller/AuthControllerTest.java
@@ -3,8 +3,11 @@ package de.lindatroesken.backend.controller;
 import de.lindatroesken.backend.api.AccessToken;
 import de.lindatroesken.backend.api.Credentials;
 import de.lindatroesken.backend.api.User;
+import de.lindatroesken.backend.config.JwtConfig;
 import de.lindatroesken.backend.model.UserEntity;
 import de.lindatroesken.backend.repo.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,18 +42,21 @@ class AuthControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JwtConfig jwtConfig;
+
     @BeforeEach
     public void initializeDB(){
 
         UserEntity user1 = UserEntity.builder()
-                .username("linda")
+                .username("testuser")
                 .password("$2a$10$3k.oydYW4Tvi2kpz7twi2.J.bXQWudC4.jEeYz7eScdedmNrg7chG")
                 .role("user")
                 .build();
         userRepository.save(user1);
 
         UserEntity user2 = UserEntity.builder()
-                .username("admin")
+                .username("testadmin")
                 .password("$2a$10$3k.oydYW4Tvi2kpz7twi2.J.bXQWudC4.jEeYz7eScdedmNrg7chG")
                 .role("admin")
                 .build();
@@ -63,23 +70,25 @@ class AuthControllerTest {
     }
 
     @Test
-    @DisplayName("POST should return token (still a hard coded token, to be replaced!)")
-    public void testPostReturnsAccessToken(){
+    @DisplayName("POST with valid user credentials should return JWT token")
+    public void testPostValidLoginReturnsAccessToken(){
         //GIVEN
         String url = getUrl() + "/access_token";
         Credentials credentials = Credentials.builder()
-                .username("linda")
+                .username("testuser")
                 .password("1234").build();
+        HttpEntity httpEntity = new HttpEntity(credentials);
+
         // WHEN
-        ResponseEntity<AccessToken> response = restTemplate.postForEntity(url, credentials, AccessToken.class);
+        ResponseEntity<AccessToken> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, AccessToken.class);
 
         // THEN
         assertThat(response.getStatusCode(), is(HttpStatus.OK));
         assertNotNull(response.getBody());
         String token = response.getBody().getToken();
-        assertThat(token, is("1234"));
-
-
+        Claims claims = Jwts.parser().setSigningKey(jwtConfig.getSecret())
+                .parseClaimsJws(token).getBody();
+        assertThat(claims.getSubject(), is("testuser"));
     }
 
 
