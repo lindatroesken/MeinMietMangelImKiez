f98ef4e9ed686467c0e21641523c299c9b2fc593
lindatrosken <linda.troesken@gmx.net>
2021-09-09 09:00:19 +0200

    Refactor App and Login: get token on submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import { getToken } from './services/api-service'
+import { useState } from 'react'
+import jwt from 'jsonwebtoken'
 
 export default function App() {
+  const [token, setToken] = useState()
+  const claims = jwt.decode(token)
+
+  const user = claims && {
+    username: claims.sub,
+    avatar: claims.avatar,
+    role: claims.role,
+  }
+  const login = credentials => getToken(credentials).then(setToken)
+
   return (
     <Router>
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route path="/login">
+          <Login onLogin={login} token={token} user={user} />
+        </Route>
       </Switch>
     </Router>
   )
