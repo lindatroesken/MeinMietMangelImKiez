87db573848fdf7daa420a725f85d259fde3894f6
lindatrosken <linda.troesken@gmx.net>
2021-09-13 21:52:31 +0200

    Refactor Frontend to provide login information on each page by using  AuthProvider and AuthContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,25 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
-import { getToken } from './services/api-service'
-import { useState } from 'react'
-import jwt from 'jsonwebtoken'
 import Home from './pages/Home'
 import ProtectedRoute from './auth/ProtectedRoute'
 import MaengelForm from './pages/MaengelForm'
 import PersonalMaengelList from './pages/PersonalMaengelList'
+import AuthProvider, { useAuth } from './auth/AuthProvider'
 
 export default function App() {
-  const [token, setToken] = useState()
-  const claims = jwt.decode(token)
-
-  const user = claims && {
-    username: claims.sub,
-    role: claims.role,
-  }
-  const login = credentials => getToken(credentials).then(setToken)
-
   return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login onLogin={login} token={token} user={user} />
-        </Route>
-        <Route exact path="/">
-          <Home user={user} />
-        </Route>
-        {user && (
-          <ProtectedRoute user={user} path={`/${user.username}/maengel/new`}>
-            <MaengelForm user={user} token={token} />
-          </ProtectedRoute>
-        )}
-        {user && (
-          <ProtectedRoute user={user} path={`/${user.username}/maengel/list`}>
-            <PersonalMaengelList user={user} token={token} />
-          </ProtectedRoute>
-        )}
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <ProtectedRoute path="/maengel/new" component={MaengelForm} />
+          <ProtectedRoute
+            path="/maengel/list"
+            component={PersonalMaengelList}
+          />
+        </Switch>
+      </Router>
+    </AuthProvider>
   )
 }
