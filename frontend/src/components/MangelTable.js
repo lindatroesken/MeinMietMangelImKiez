8347fdd7d8cf4360f692398c4e92fcab5cfb8a22
lindatrosken <linda.troesken@gmx.net>
2021-09-14 10:25:14 +0200

    Remove DetailsLink, because complete row will be clickable

--- frontend/src/components/MangelTable.js
+++ frontend/src/components/MangelTable.js
@@ -2,8 +2,6 @@ import React, { useMemo } from 'react'
 
 import Table from './Table'
 import TimeAgo from 'react-timeago'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components/macro'
 
 export default function MangelTable({ data }) {
   const columns = useMemo(
@@ -21,9 +19,6 @@ export default function MangelTable({ data }) {
           {
             Header: 'Beschreibung',
             accessor: 'description',
-            Cell: ({ cell: { value } }) => (
-              <DetailsLink to={`/mangel/details/${value}`}>{value}</DetailsLink>
-            ),
           },
         ],
       },
@@ -33,8 +28,3 @@ export default function MangelTable({ data }) {
 
   return <Table columns={columns} data={data} />
 }
-
-const DetailsLink = styled(Link)`
-  text-decoration: none;
-  color: var(--neutral-dark);
-`
