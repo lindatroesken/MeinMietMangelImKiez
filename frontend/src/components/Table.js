a48b7a774a946e86f4f7b4bf8f8dfd186b1f394c
lindatrosken <linda.troesken@gmx.net>
2021-09-15 09:14:53 +0200

    Formatting

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTable } from 'react-table'
 import { useHistory } from 'react-router-dom'
+
 export default function Table({ columns, data }) {
   const history = useHistory()
 
