cf24f8a5b22c2fcc5b03783376f3e7d9de6dc78e
lindatrosken <linda.troesken@gmx.net>
2021-09-16 12:09:23 +0200

    Add table contact_logger to database, add entity

---
+++ backend/src/main/java/de/lindatroesken/backend/model/ContactLoggerEntity.java
@@ -0,0 +1,36 @@
+package de.lindatroesken.backend.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "contact_logger")
+@Setter
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactLoggerEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "mangel")
+    private MangelEntity mangelEntity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "contact_type")
+    private ContactType contactType;
+
+    @Column(name = "date_contacted")
+    private ZonedDateTime dateContacted;
+
+    @Column(name = "contact_note")
+    private String contactNote;
+
+}
