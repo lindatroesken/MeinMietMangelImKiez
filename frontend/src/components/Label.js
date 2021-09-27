8c5502c3f2431054e8cf9bced9fc25a59a4b023e
lindatrosken <linda.troesken@gmx.net>
2021-09-27 19:52:26 +0200

    Styling: label left and margin above label

--- frontend/src/components/Label.js
+++ frontend/src/components/Label.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/macro'
 
 export default styled.label`
-  text-align: center;
+  text-align: left;
   width: 100%;
   color: var(--neutral-dark);
+  margin-top: var(--size-m);
 `
