dd8368d4c4229b83d46a9066522adf6a3878460a
lindatrosken <linda.troesken@gmx.net>
2021-10-07 13:11:23 +0200

    Refactor frontend test message

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
-import { render } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-});
+test('renders mietmangel frontend without crashing', () => {
+  render(<App />)
+})
